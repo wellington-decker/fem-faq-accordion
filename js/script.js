03c9e31cdc1a87ad1db9d0dc278ffbeedd80efe8
Decker Wellington <deckerwellington@yahoo.com>
2022-07-22 11:43:11 +0100

    I committed all the files used

---
+++ js/script.js
@@ -0,0 +1,78 @@
+const details = document.querySelectorAll("details")
+const summary = document.querySelector("summary");
+const arrow = document.querySelector(".arrow");
+const accordionHeading = document.querySelector(".accordion-heading");
+
+
+
+
+// details.forEach(detail => {
+//   detail.addEventListener("click", () => {
+//     let i;
+//     for (i = 0; i < arrow.length; i++) {
+//       arrow[i].classList.toggle("flip");
+//     // arrow.forEach(arr => {
+//     //   arr.classList.toggle("flip");
+//     }
+//   }) 
+// })
+
+
+// let arrow = document.querySelectorAll(".arrow");
+// let i;
+
+// for (i = 0; i < arrow.length; i++) {
+//   arrow[i].addEventListener("click", function() {
+//     this.classList.toggle("flip");
+//   });
+//   accordionHeading[i].addEventListener("click", function() {
+//     this.classList.toggle("show");
+//   });
+   
+  
+
+// }
+
+
+
+// let accordionHeading = document.getElementsByClassName("accordion-heading");
+// let j;
+
+// for (j = 0; j < accordionHeading.length; j++) {
+//   accordionHeading[j].addEventListener("click", function() {
+//     this.classList.toggle("show");
+
+//   });
+// }
+
+
+
+
+
+// const openAccordion = () => {
+//     arrow.forEach(arrow_ => {
+//         arrow_.classList.toggle("flip");
+//     })
+//     accordionHeading.forEach(header => {
+//         header.classList.toggle("show");
+//     })  
+// }
+
+
+//2
+
+// toggleAccordion.forEach(penAcc => {
+//     penAcc.addEventListener("click", () => {
+//         arroq.classList.toggle("flip")
+//         accordionHeading.classList.toggle("show");
+        
+//     })
+    
+// });
+
+
+// toggleAccordion.addEventListener("click", openAccordion);
+
+
+
+
