5e1cd1088cc446317c269e793fbbd7987508f7f7
Decker Wellington <deckerwellington@yahoo.com>
2022-07-22 12:40:37 +0100

    I completed the function to toggle the header and add the appropriate effects to the childNodes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-const details = document.querySelectorAll("details")
+const details = document.querySelectorAll("details");
 const summary = document.querySelector("summary");
 const arrow = document.querySelector(".arrow");
 const accordionHeading = document.querySelector(".accordion-heading");
+
+details.forEach((detail) => {
+  detail.addEventListener("toggle", (e) => {
+    let getChildren = e.target.children;
+    getChildren[0].firstElementChild.classList.toggle("show");
+    getChildren[0].firstElementChild.classList.remove("remove-hover");
+    getChildren[0].lastElementChild.classList.toggle("flip");
+  });
+});
+
 
